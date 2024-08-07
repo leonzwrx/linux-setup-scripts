87a23de4e58efc6abbfb1bab6bc46671ff93835e
leonzwrx <158376547+leonzwrx@users.noreply.github.com>
2024-08-07 10:37:42 -0600

    minor

--- _debian_extras_bookworm.sh
+++ _debian_extras_bookworm.sh
@@ -24,7 +24,7 @@ sudo apt install -y ranger ncdu psmisc mangohud cpu-x iftop iotop btop powertop 
 # sudo apt install -y bluez blueman bluetooth
 
 # Sounds and multimedia
-sudo apt install -y gimp imagemagick celluloid cmus cava ffmpeg ffmpegthumbnailer kid3-qt pactl lximage-qt
+sudo apt install -y gimp imagemagick celluloid cmus cava ffmpeg ffmpegthumbnailer pactl lximage-qt
 # github.com/Pinaki82/debian-minimal lists a bunch of gimp goodies to install
 
 # PDF, printing and scanning
@@ -64,7 +64,7 @@ sudo apt install -y onlyoffice-desktopeditors
 # Install your preferred Flatpaks (modify according to your needs)
 flatpak update
 flatpak install -y flathub com.github.tchx84.Flatseal io.github.prateekmedia.appimagepool com.github.qarmin.czkawka it.mijorus.gearlever \
-  com.github.PintaProject.Pint aio.github.shiftey.Desktop org.nicotine_plus.Nicotine io.podman_desktop.PodmanDesktop com.vixalien.sticky
+  com.github.PintaProject.Pint aio.github.shiftey.Desktop org.nicotine_plus.Nicotine io.podman_desktop.PodmanDesktop com.vixalien.sticky org.kde.kid3
 # OPTIONAL - Install auto-cpufreq if laptop
 #  cd ~/Applications
 #  git clone https://github.com/AdnanHodzic/auto-cpufreq.git
